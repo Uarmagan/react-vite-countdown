1ef766adca620fe182b9bae064aa3b2e8c3c6dc6
Ugur Armagan <ugurarmagan93@gmail.com>
2021-08-04 00:20:26 -0500

    adds initial countdown timer

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,74 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import styled from "styled-components";
+import { ITimeLeft } from "./types";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeLeft(calculateTimeLeft);
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, [timeLeft]);
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+        <h2>We're Launching Soon</h2>
+        <p>countdown</p>
+        <div className="countdown">
+          <$CountdownWrapper>
+            <div className="days">
+              <p>Days</p>
+              <p>{timeLeft.days}</p>
+            </div>
+            <div className="hours">
+              <p>Days</p>
+              <p>{timeLeft.hours}</p>
+            </div>
+            <div className="minutes">
+              <p>Days</p>
+              <p>{timeLeft.minutes}</p>
+            </div>
+            <div className="seconds">
+              <p>Days</p>
+              <p>{timeLeft.seconds}</p>
+            </div>
+          </$CountdownWrapper>
+        </div>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+const calculateTimeLeft = (): ITimeLeft => {
+  let year = new Date().getFullYear();
+  const difference: number = +new Date(`10/01/${year}`) - +new Date();
+
+  if (difference > 0) {
+    return {
+      days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+      hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+      minutes: Math.floor((difference / 1000 / 60) % 60),
+      seconds: Math.floor((difference / 1000) % 60),
+    };
+  }
+
+  return {
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  };
+};
+
+const $CountdownWrapper = styled.div`
+  display: flex;
+  justify-content: space-around;
+  width: 500px;
+`;
+
+export default App;
